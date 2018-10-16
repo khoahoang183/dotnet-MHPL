9af29e5c125634c92e312b1d9fa171b61f4ad106
iohkhnim <44048705+iohkhnim@users.noreply.github.com>
2018-10-16 11:28:49 +0700

    add phieuthu

---
+++ Project/QuanLyBanVeSo/Model/DAO/PhieuThuDAO.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EF;
+using PagedList;
+using System.Data;
+
+namespace Model.DAO
+{
+    public class PhieuThuDAO
+    {
+        QuanLyBanVeSoDbContext db = null;
+        public PhieuThuDAO()
+        {
+            db = new QuanLyBanVeSoDbContext();
+        }
+
+        public PHIEUTHU GetByID(int MaPT)
+        {
+            return db.PHIEUTHUs.SingleOrDefault(x=> x.MAPT == MaPT);
+        }
+
+        public PHIEUTHU ViewDetail(int MaPT)
+        {
+            return db.PHIEUTHUs.Find(MaPT);
+        }
+
+        public List<PHIEUTHU> GetAll()
+        {
+            return db.PHIEUTHUs.ToList();
+        }
+        public IEnumerable<PHIEUTHU> listAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<PHIEUTHU> model = db.PHIEUTHUs;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.MADL.ToString().Contains(searchString));
+            }
+            return model.OrderBy(x => x.MAPT).ToPagedList(page, pageSize);
+        }
+
+        public long Insert(PHIEUTHU entity)
+        {
+            entity.TINHTRANG = 1;
+            db.PHIEUTHUs.Add(entity);
+            db.SaveChanges();
+            return entity.MAPT;
+        }
+
+        public bool Update(PHIEUTHU entity)
+        {
+            try
+            {
+                var phieuthu = db.PHIEUTHUs.Where(g => g.MAPT == entity.MAPT).SingleOrDefault();
+                phieuthu.MADL = entity.MADL;
+                phieuthu.NGAY = entity.NGAY;
+                phieuthu.TIEN = entity.TIEN;
+                phieuthu.TINHTRANG = entity.TINHTRANG;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Project/QuanLyBanVeSo/QuanLyBanVeSo/Controllers/PhieuThuController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model.EF;
+using Model.DAO;
+
+namespace QuanLyBanVeSo.Controllers
+{
+    public class PhieuThuController : Controller
+    {
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 8)
+        {
+            var dao = new Model.DAO.PhieuThuDAO();
+            var model = dao.listAllPaging(searchString, page, pageSize);
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            SetViewBag();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            SetViewBag();
+            var phieuthu = new PhieuThuDAO().ViewDetail(id);
+            return View(phieuthu);
+        }
+
+        [HttpPost]
+        public ActionResult Create(PHIEUTHU phieuthu)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new Model.DAO.PhieuThuDAO();
+                phieuthu.NGAY = DateTime.Today;
+                long id = dao.Insert(phieuthu);
+                if (id > 0) // if insert success, back to index
+                {
+                    return RedirectToAction("Index", "PhieuThu");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thêm Số Lượng Đăng Ký Thất Bại!");
+                }
+            }
+            return View("Index");
+        }
+        [HttpPost]
+        public ActionResult Edit(PHIEUTHU phieuthu)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new Model.DAO.PhieuThuDAO();
+                var result = dao.Update(phieuthu);
+                if (result) // if update success, back to index
+                {
+                    return RedirectToAction("Index", "PhieuThu");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Cập Nhật Thất Bại!");
+                }
+            }
+            return View("Index");
+        }
+
+        public void SetViewBag(long? selectedID = null)
+        {
+            var dao = new DaiLyDAO();
+            ViewBag.MaDL = new SelectList(dao.GetAll(), "MaDL", "MaDL", selectedID);
+        }
+    }
+}
